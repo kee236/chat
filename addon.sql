18b89aea1e4d4d3ad42a3750f94ceb438ae24740
kee236 <31869356+kee236@users.noreply.github.com>
2024-09-04 06:22:45 +0700

    Update

---
+++ addon.sql
@@ -0,0 +1,5 @@
+INSERT INTO `add_ons` (`id`, `add_on_name`, `unique_name`, `version`, `installed_at`, `update_at`, `purchase_code`, `module_folder_name`, `project_id`) VALUES 
+
+
+(43, 'Flowbuilder Conditional Reply', 'flowbuilder_condition', '1.0', '2024-09-01 10:11:26', '2024-09-01 10:11:26', '', 'flowbuilder_condition', 63),
+(46, 'Visual Flow Builder', 'visual_flow_builder', '1.6.9', '2024-09-01 10:17:27', '2024-09-01 10:17:27', '', 'visual_flow_builder', 59);
