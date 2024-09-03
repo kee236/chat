a29693828743d7085a588dd6b7f7e27848f384dc
kee236 <31869356+kee236@users.noreply.github.com>
2024-09-04 06:33:15 +0700

    Update addon.sql

--- addon.sql
+++ addon.sql
@@ -1,9 +1,11 @@
 INSERT INTO `add_ons` (`id`, `add_on_name`, `unique_name`, `version`, `installed_at`, `update_at`, `purchase_code`, `module_folder_name`, `project_id`) VALUES 
 
 
-(17, 'User Input & Custom Fields', 'custom_field_manager', '2.0.1', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', 'custom_field_manager', 49),
-(17, 'Flowbuilder Conditional', 'flowbuilder_condition', '1.0', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', 'flowbuilder_condition', 63),
+(31, 'User Input & Custom Fields', 'custom_field_manager', '2.0.1', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', 'custom_field_manager', 49),
+(32, 'Flowbuilder Conditional', 'flowbuilder_condition', '1.0', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', 'flowbuilder_condition', 63),
 (25, 'Google My Business Made Easy', 'gmb', '1.0', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', 'gmb', 55);
 
-(43, 'Flowbuilder Conditional Reply', 'flowbuilder_condition', '1.0', '2024-09-01 10:11:26', '2024-09-01 10:11:26', '', 'flowbuilder_condition', 63),
+(33, 'Flowbuilder Conditional Reply', 'flowbuilder_condition', '1.0', '2024-09-01 10:11:26', '2024-09-01 10:11:26', '', 'flowbuilder_condition', 63);
+
+
 (46, 'Visual Flow Builder', 'visual_flow_builder', '1.6.9', '2024-09-01 10:17:27', '2024-09-01 10:17:27', '', 'visual_flow_builder', 59);
