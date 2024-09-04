03b906fabeb9700605bbee24bb03b67b76fc2f87
kee236 <31869356+kee236@users.noreply.github.com>
2024-09-05 01:34:32 +0700

    Update addon.sql

--- addon.sql
+++ addon.sql
@@ -1,6 +1,9 @@
 INSERT INTO `add_ons` (`id`, `add_on_name`, `unique_name`, `version`, `installed_at`, `update_at`, `purchase_code`, `module_folder_name`, `project_id`) VALUES 
 
 
+
+
+
 (31, 'User Input & Custom Fields', 'custom_field_manager', '2.0.1', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', 'custom_field_manager', 49),
 (32, 'Flowbuilder Conditional', 'flowbuilder_condition', '1.0', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', 'flowbuilder_condition', 63),
 (25, 'Google My Business Made Easy', 'gmb', '1.0', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', 'gmb', 55);
